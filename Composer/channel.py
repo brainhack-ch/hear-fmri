e0419cdafb504d18314a1c06cbb6ef6e4163864d
Emma Tolley <etolley@Emmas-MacBook-Pro.local>
2019-11-09 16:47:21 +0100

    final files & code

--- Composer/channel.py
+++ Composer/channel.py
@@ -1,26 +1,5 @@
 import pygame
-'''
-class MidiChannel:
-	#Create file
-	def __init__(self, channel, input_midi):
-		self.channel = channel
-		out_file = open(data_folder + str(channel)+'.mid', 'wb')
-		self.output_midi = MidiOutFile(out_file)
-		self.input_midi = MidiInFile(MidiToCode(), input_midi)
-​
-	def store_channels(self, channel_sig):
-		self.signal = channel_signal
-		
-	def process(self):
-		self.midi.header(format=0, nTracks=1, division=96)
-		self.midi.start_of_track()
-		self.midi.update_time(0)
-		for idx, val in enumerate(self.signal):
-			if(val == 1):
-				self.midi.note_on(idx, 0x40, 0x64)
-				self.midi.update_time(idx+50)
-				self.midi.note_off(0, 0x40, 0x40)
-'''
+
 class WavChannel:
 	#Create file
 	def __init__(self, channel, name = ''):
@@ -70,6 +49,15 @@ class WavChannel:
 				return "{0} ({1:.2f})".format(self.name,self.signal[idx])
 			else:
 				self.sound.fadeout(50)
+		elif self.mode == 'trigger':
+			#print(self.channel, self.signal[idx])
+			if self.signal[idx] > 0:
+				#self.sound.set_volume(self.signal[idx])
+				if not self.isPlaying():
+					self.voice.play(self.sound)
+				return "{0} ({1:.2f})".format(self.name,self.signal[idx])
+			else:
+				self.sound.fadeout(2000)
 		else:
 			self.voice.play(self.sound)
 			return self.name
--- Composer/make_music.py
+++ Composer/make_music.py
@@ -14,44 +14,106 @@ from matplotlib.collections import PatchCollection
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from scipy import interpolate
+from scipy.io.wavfile import write
+import sounddevice as sd
 
 
 basic_sounds = ["Test_sounds/A.wav", "Test_sounds/Bb.wav","Test_sounds/C.wav",
 	           "Test_sounds/D.wav","Test_sounds/E.wav","Test_sounds/F.wav",
 	           "Test_sounds/G.wav"]
 drum_sounds = ["drum_sounds/Hi-hat 1 .wav",
+			  "drum_sounds/Kick.wav",
                "drum_sounds/Hi-hat 2 .wav",
-               "drum_sounds/Hi-hat 3 .wav",
-               "drum_sounds/Kick.wav",
+               #"Test_sounds/F.wav",
+               #"Dynamic Composition samples/WeeWowWoo - Osc 3.wav",
+               
                "drum_sounds/Snare .wav",
-               "Test_sounds/A.wav", "Test_sounds/Bb.wav",]
+               "drum_sounds/Hi-hat 3 .wav",
+               
+               "Dynamic Composition samples/Bassline - Osc 3.wav",
+               #"Test_sounds/A.wav"
+               ]
+
+drum_sounds = ["drum_sounds/Hi-hat 1 .wav",
+			  "drum_sounds/Kick.wav",
+               "drum_sounds/Hi-hat 2 .wav",
+               #"Test_sounds/F.wav",
+               #"Dynamic Composition samples/WeeWowWoo - Osc 3.wav",
+               
+               "drum_sounds/Snare .wav",
+               "drum_sounds/Hi-hat 3 .wav",
+               
+               "Dynamic Composition samples/Bassline - Osc 3.wav",
+               #"Test_sounds/A.wav"
+               ]
+
+dynamic_sounds = [
+ "Dynamic Composition samples/Bassline - Osc 3.wav",
+"Dynamic Composition samples/Blooooooop - osc 3.wav",
+"Dynamic Composition samples/Creaker - Osc 1.wav",
+"Dynamic Composition samples/Grr grr grr - Osc 5.wav",
+"Dynamic Composition samples/Meow - Osc 6.wav",
+"Dynamic Composition samples/Screamer - osc 2.wav",
+"Dynamic Composition samples/WeeWowWoo - Osc 3.wav",]
+
+fear_sounds = ["drum_sounds/Kick.wav",
+ "Dynamic Composition samples/Bassline - Osc 3.wav",
+"Dynamic Composition samples/Blooooooop - osc 3.wav",
+"Dynamic Composition samples/Creaker - Osc 1.wav",
+"Dynamic Composition samples/Grr grr grr - Osc 5.wav",
+"Dynamic Composition samples/Meow - Osc 6.wav",
+"Dynamic Composition samples/Screamer - osc 2.wav",
+"Dynamic Composition samples/WeeWowWoo - Osc 3.wav",]
+
+vocal_sounds = [
+"Bounce Vocal chops/Camille - Sensory Motor.wav",
+"Bounce Vocal chops/Charlotte  - Default .wav",
+"Bounce Vocal chops/Emma - Frontal Parietal .wav",
+"Bounce Vocal chops/Julien - Ventral .wav",
+"Bounce Vocal chops/Paula - Dorsal attentional .wav",
+"Bounce Vocal chops/Raphael - Sight.wav",
+"Bounce Vocal chops/Thomas - Limbic.wav",
+
+]
 
 def playSongAndAnimation(music_array, nchannels, interval, mode, smoothingfactor = 2):
-	BLACK = (0, 0, 0)
-	WHITE = (255, 255, 255)
-	GREEN = (0, 255, 0)
-	RED   = (255, 0, 0)
+	fig = plt.figure()
+	ax = plt.axes(xlim=(-10, 10), ylim=(-10, 10))
+	plt.gca().set_aspect('equal', adjustable='box')
+	frame1 = plt.gca()
+	frame1.axes.get_xaxis().set_ticks([])
+	frame1.axes.get_yaxis().set_ticks([])
+	line, = ax.plot([], [], lw=5)
 
-	background_colour = (255,255,255)
-	(width, height) = (300, 300)
-	screen = pygame.display.set_mode((width, height))
-	pygame.display.set_caption('A E S T H E T I C')
-	pygame.display.update()
- 
-	# Used to manage how fast the screen updates
-	clock = pygame.time.Clock()
 
-	myFont = pygame.font.SysFont('arial', 14)
+	def init():
+	    line.set_data([], [])
+	    return line,
 
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				sys.exit()
+	def animate(i):
+		#t = np.linspace(0, 1, 20)
+		#x = (5)*np.cos(2 * np.pi * (t - 0.01 * i))
+		#y = (5)*np.sin(2 * np.pi * (t - 0.01 * i))
+		x = []
+		y = []
+		for t in np.linspace(0, 1, 20):
+			r = 5 + 3*np.sin(2*np.pi*t*2- 0.05 * i)
+			xpt = r*np.cos(2 * np.pi * (t - 0.001 * i))
+			ypt = r*np.sin(2 * np.pi * (t - 0.001 * i))
+			x.append(xpt)
+			y.append(ypt)
+		#tck, u = interpolate.splprep([x, y], s=0)
+		#unew = np.arange(0, 1.01, 0.01)
+		#out = interpolate.splev(unew, tck)
+		#line.set_data(out[0], out[1])
+		line.set_data(x, y)
+		return line,
 
-		pygame.draw.circle(screen, GREEN, (0,0), 20) 
-		pygame.display.update()
-		clock.tick(60)
+
+	# call the animator.  blit=True means only re-draw the parts that have changed.
+	anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=len(music_array[0]), interval=20, blit=True)
+	plt.show()
 	 
 	# Close the window and quit.
 	pygame.quit()
@@ -60,7 +122,7 @@ def playSongAndAnimation(music_array, nchannels, interval, mode, smoothingfactor
 def initialiseChannels(nchannels, sounds, data_array, mode):
 	pygame.mixer.set_num_channels(nchannels)
 	channels = []
-	for i in range(len(data_array)):
+	for i in range(nchannels):
 		print ("Setting channel data to", data_array[i])
 		thischannel = WavChannel(i, sounds[i].split('/')[-1].split('.')[0] )
 		thischannel.store_channels(data_array[i])
@@ -75,7 +137,7 @@ def playSong(music_array, nchannels, interval, mode = "binary"):
 
 	nsteps = len(music_array[0])
 
-	channels = initialiseChannels(nchannels, drum_sounds, music_array, mode)
+	channels = initialiseChannels(nchannels, fear_sounds, music_array, mode)
 
 	for i in range(nsteps):
 		notes = []
@@ -88,23 +150,43 @@ def playSong(music_array, nchannels, interval, mode = "binary"):
 if __name__ == "__main__":
 	print("Now running!")
 	pygame.init() 
-	interval = 72 # milliseconds
-	nchannels = 7
+	interval = 720 # milliseconds
+	nchannels = 8
 
-	filename ='test_data/matrix_network.mat'
-	dataname = 'matrix_network'
+	#filename ='test_data/matrix_network.mat'
+	#dataname = 'matrix_network'
 
 	#filename = 'test_data/peaks.mat'
 	#dataname = 'signals_max'
 
+	#filename = 'test_data/TIMECOURSES_THRESHOLDED.mat'
+	#dataname = 'TC_positive'
+	#dataname = "TC_negative"
+
+	#filename = "test_data/output_fear 100307.mat"
+	#dataname = "output_fear"
+
+	filename = "test_data/output 100307final.mat"
+	dataname = "output_fear"
+
 	print ("Reading {0} from {1}".format(dataname, filename))
 
 	fmridata = loadmat(filename)
+	print (fmridata)
+	#sys.exit()
 	music_data = fmridata[dataname]
 
-	if not len(music_data) == nchannels:
-		raise ValueError("Incorrect number of channels!")
+	if len(music_data) < nchannels:
+		raise ValueError("Not enough channels! Expected {0} and got {1}".format(nchannels, len(music_data) ))
 
-	playSongAndAnimation(music_data, nchannels, interval, mode = "binary")
+	#playSongAndAnimation(music_data, nchannels, interval, mode = "binary")
 	
-	#playSong(music_data, nchannels, interval, mode = "binary")
+	#fs = 44100
+	#duration = 10.5  # seconds
+	#sd.default.samplerate = 44100
+	#sd.default.device = 'digital output'
+	#myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=2)
+	playSong(music_data, nchannels, interval, mode = "modulate")
+	#sd.wait()
+	#write('output.wav', fs, myrecording)
+
